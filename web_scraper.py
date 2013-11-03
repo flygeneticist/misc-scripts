24ea834aa792ec4e559cb981a9ecf10cb079301d
Kevin Keller <kak9699@crunchbang.(none)>
2013-11-03 14:30:05 -0500

    removed test lines for url input

--- web_scraper.py
+++ web_scraper.py
@@ -5,7 +5,7 @@
 
 # import need modules
 from bs4 import BeautifulSoup
-import urllib, re, os, csv
+import urllib, re, sys
 
 def get_user_url(): # function to get the url input from the user
   target_url = raw_input()
@@ -18,22 +18,11 @@ def grab_and_parse_results(target_url): # function to get HTML from a url and pa
   # Results should contain only Japanese sentences (Look for any Japanese characters that end with . ! ? ...)
   pattern = u'[\u3030-\u9FAF][\u3030-\u9FAF0-9A-Za-z\u3001\u2026 ]+[\u300D|\u3002|\uFF01|\uFF1F|\u2026]+'
   # Returns a list based on a set of unique sentences only (removes duplicates)
-  return list(set(re.findall(pattern, html_soup.decode('utf8'))))
-
-def save_as_csv(sentence_list): # function to handle creation, writing, and closing of CSV file
-  # Create a new CSV file to write sentences to
-  csv_output_file = open('web_scraper_file.csv', 'w')
-  # Create the csv writer object
-  file_writer = csv.writer(csv_output_file)
-  # Write each sentence scraped as a new row in the CSV file
-  file_writer.writerow("Expression") # Write in header label for first row
-  for sentence in sentence_list:
-    file_writer.writerow(sentence.encode('utf8'))
-  # Close out the CSV file when finished writing to it
-  csv_output_file.close()
+  return list(set(re.findall(pattern, html_soup.decode('utf-8-sig'))))
 
 # RUN TIME CODE:
-#target_url = get_user_url() # Get target url from the user
-target_url = 'http://rkmg.higu.biz/?a=alche&m=1&story_idx=22&list_idx=531'
-sentences = grab_and_parse_results(target_url) # Grab and parse HTML into sentences
-save_as_csv(sentences)
+target_url = get_user_url() # Get target url from the user
+list_of_sentences = grab_and_parse_results(target_url) # Grab and parse HTML into sentences
+# prints out each of the sentences to the terminal output
+for sentence in list_of_sentences:
+  print sentence
