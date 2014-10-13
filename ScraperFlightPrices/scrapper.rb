2d5dacc608f17183701a79eb59844eeb9194333e
Kevin Keller <flygeneticist@gmail.com>
2014-10-13 10:23:04 -0600

    route scraping completed. Ready for cleanup.

--- ScraperFlightPrices/scrapper.rb
+++ ScraperFlightPrices/scrapper.rb
@@ -32,16 +32,33 @@ base_url = "http://http://www.faredetective.com"
 # output.close
 
 
-# Scrape the top-level page into output to get all route links for all destinations
-output = File.open( "route_links.txt", "w" )
-input = File::open("sub_dest_links.txt", "r")
-input.each do |dest|
-    url = dest.chomp[0..-2]
-    Nokogiri::HTML(open(url)).css("td:nth-child(1) > a").each do |l|   
-        route_link = base_url + "#{l}"[/\/farehistory\/.*\.html/] + ",\n"
-        output << route_link
-    end
-    puts "Scraped: #{url}"
+# # Scrape the top-level page into output to get all route links for all destinations
+# output = File.open( "route_links.txt", "w" )
+# input = File::open("sub_dest_links.txt", "r")
+# input.each do |dest|
+#     url = dest.chomp[0..-2]
+#     Nokogiri::HTML(open(url)).css("td:nth-child(1) > a").each do |l|   
+#         route_link = base_url + "#{l}"[/\/farehistory\/.*\.html/] + ",\n"
+#         output << route_link
+#     end
+#     puts "Scraped: #{url}"
+# end
+# input.close
+# output.close
+
+## Post processing of route data to ensure clean/unique links
+# push route links data into a set to ensure no duplicate links
+input = File.open( "route_links.txt", "r" )
+unique_routes = Set.new
+input.each do |link|
+    unique_routes << link
 end
 input.close
+puts "Adding route links to file..."
+# write unique links in set out to a new file
+output = File.open( "route_links_clean.txt", "w" )
+unique_routes.each do |link|
+    output << link
+end
 output.close
+puts "Cleaning of route links is complete!"
