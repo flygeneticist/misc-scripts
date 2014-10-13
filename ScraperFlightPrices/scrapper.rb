5485d310fa71a579ae557ac4ec45da097bfa56b9
Kevin Keller <flygeneticist@gmail.com>
2014-10-13 09:54:36 -0600

    scraper pulled all dest links and sub-pages. Ready to scrape all route links.

--- ScraperFlightPrices/scrapper.rb
+++ ScraperFlightPrices/scrapper.rb
@@ -13,27 +13,34 @@ base_url = "http://http://www.faredetective.com"
 # end
 # output.close
 
-# scrape each popular destination page and all subpages to get links for specific routes
-output = File.open( "route_links.txt", "w" )
-input = File::open("dest_links_test.txt", "r")
+
+# # scrape each popular destination page and all subpages to get sub-page links 
+# output = File.open( "sub_dest_links.txt", "w" )
+# input = File::open("dest_links.txt", "r")
+# input.each do |dest|
+#     output << dest
+#     url = dest.chomp[0..-2]
+#     # Check for sub-pages and output URLs if present
+#     pg_links = Nokogiri::HTML(open(url)).css("div.div4 > a")
+#     pg_links[0..-2].each do |l|
+#         output << url + "#{l}"[/#[1-9][0-9]?/] + ",\n"
+#     end
+#     sleep 1.0 + rand
+#     puts "Scraped: #{url}"
+# end
+# input.close
+# output.close
+
+
+# Scrape the top-level page into output to get all routes links for all destinations
+output = File.open( "routes_links.txt", "w" )
+input = File::open("sub_dest_links.txt", "r")
 input.each do |dest|
-    # Scrape the first top-level page into output
     url = dest.chomp[0..-2]
     Nokogiri::HTML(open(url)).css("td:nth-child(1) > a").each do |l|   
-       output << base_url + "#{l}"[/\/farehistory\/.*\.html/] + ",\n"
-    end
-
-    # Check for sub-pages and crawl if present
-    pg_links = Nokogiri::HTML(open(url)).css("div.div4 > a")
-    if pg_links.length >= 2
-        pg_links[0..-2].each do |sl|
-            subpg = url + "#{sl}"[/#[1-9][0-9]?/]
-            Nokogiri::HTML(open(subpg)).css("td:nth-child(1) > a").each do |subl|
-                puts subl  
-                output << base_url + "#{subl}"[/\/farehistory\/.*\.html/] + ",\n"
-            end
-        end
-    end
+    route_link = base_url + "#{l}"[/\/farehistory\/.*\.html/] + ",\n"
+    output << route_link
+    puts route_link
 end
 input.close
 output.close
