f69b8870fd4fb6f1892de8c0bfe2f0fc4349849c
Rob Cole <robcole@gmail.com>
2014-10-18 21:56:01 -0600

    Update travel_scraper_mobtravel.rb
    Build array of hashes from scraped data, then JSON.parse into JSON object

--- hotel_scraper/travel_scraper_mobtravel.rb
+++ hotel_scraper/travel_scraper_mobtravel.rb
@@ -1,14 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require 'json'
+
 # require 'pry'
 
 base_url = "http://www.travelmob.com/vacation-rentals"
 ctry = {"vietnam" => 26,"cambodia" => 7,"thailand" => 125}
 
 ## grab all broad links to popular destinations
-output = File::open("travelmob_data.txt", "w")
-output << '['
-
+results = []
 ctry.each do |k,v|
     (1..v).each do |pg|
         url = "#{base_url}/#{k}?page=#{pg.to_s}"
@@ -18,19 +18,23 @@ ctry.each do |k,v|
             price = box.css('.price').text
             bedrooms = box.css('.bedrooms').text
             capacity = box.css('.accommodates').text
-            output << "{'country'   : #{k},
-                        'city'      : #{city},
-                        'name'      : #{name},
-                        'price'     : #{price},
-                        'bedrooms'  : #{bedrooms},
-                        'capacity'  : #{capacity}
-                    },"
+
+            results << {
+                "country" => k,
+                "city" => city,
+                "name" => name,
+                "price" => price.to_i,
+                "bedrooms" => bedrooms,
+                "capacity" => capacity
+            }
+
         end
         puts "I just scraped the F%!@* out of #{k} - pg #{pg}"
         sleep 1.0 + rand # sleep to avoid hitting the server too frequently
     end
 end
 
-output << "]"
+output = File::open("travelmob_data.txt", "w")
+output << results.to_json
 output.close
 puts "Done! ^____^ "
